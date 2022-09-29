a5c42d087cfdaa9f95f94a810540f5ba8e29fb52
AlfredDaimari <alfreddaimari77@gmail.com>
2022-09-29 18:04:35 +0530

    2nd init commit

---
+++ uptane/roles/root.py
@@ -0,0 +1,28 @@
+#
+# Root service
+#   - root service keys need to be updated and used the least amongst all services
+#   - will generate root_metadata.toml file (using toml files to make it human readable)
+#
+
+class Root:
+
+    def __init__(self) -> None:
+        pass
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
