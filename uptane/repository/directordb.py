d5f9a6d885281b93b9a6f6a6163decd49e405867
AlfredDaimari <alfred_daimari@yahoo.com>
2022-12-09 16:39:33 +0530

    fix - skeleton for manifest function

--- uptane/repository/directordb.py
+++ uptane/repository/directordb.py
@@ -1,21 +1,48 @@
 import flask 
 import os 
 import json
+import uptane.verify
+import typing
+import uuid
 
 directordb = flask.Flask(__name__)
 directordb.config["MANIFEST_JSON"] = "manifest.json"
+# read json file -> dict
+
+def get_update_manifest(manifest:typing.Any)->bool:
+    '''
+    Function that returns the next manifest by reading the manifest.json file
+        - if no new update is present, returns False, else true
+        - updates manifest in place
+    '''
+# manifest return structure
+#   {
+#       ecu1:{
+#       image_name:     
+#       image_url: http://ip:port/repo/reponame/image
+#       image_size:
+#       image_hash:
+#       image_hash_func:
+#       image_buf_size:
+#       image_sig_algo:
+#       image_version:
+#       },
+#       ecu2:{}
+#
+#   }
+    return False
+
 
 # make python open up a specific directory in the filesystem
-
 # the temporary repo will be used to generate metadata files
-if not os.path.exists('./temporary_repo'):
-    os.mkdir('./temporary_repo')
+if not os.path.exists('./temp_met_dir'):
+    os.mkdir('./temp_met_dir')
 
 
 # the car should send a json in the format
 # {
 #   ecu1: {
-#   signed: { image_hash: "hash"},
+#   signed: { image_hash: "hash",},
 #   signature: "dafkljlj"
 #   },
 #   ecu2: {
@@ -25,9 +52,24 @@ if not os.path.exists('./temporary_repo'):
 #   vin: "id-number"
 # }
 #
+
+# -- this is an ad-hoc implementation [need to change it]
 @directordb.route('/manifest/', methods=["POST"])
 def manifest():
     try:
-        return ''
+        request_json = flask.request.json
+        manifest_json_dict = json.load(request_json)
+        verifier = uptane.verify.ECUVerification(manifest_json_dict)
+        verifier.verify_ecus()
+
+        if not get_update_manifest(manifest_json_dict):
+            raise Exception("upto-date")
+        
+        # make a temporary directory to put all the metadata files
+        temp_dir = str(uuid.uuid4())
+
+        if not os.path.exists(f'./temp_met_dir/{temp_dir}'):
+            os.mkdir(temp_dir)
+
     except Exception as e:
         return json.dumps({"error":{"type":str(e)}})
